7c3f68a5b708ceed05c4d5cffe7477e3f0150cf2
Ubaid Baig <urbgit.acc@gmail.com>
2012-07-31 20:44:29 +0500

    First commit - testing

---
+++ Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CheckCookieSupport();
+
+    }
+
+    private void CheckCookieSupport()
+    {
+        // Cookie detection.
+        // Is our cookie not yet set and the url parameter empty? If so, lets set the cookie.
+        if (HttpContext.Current.Session["CookieCheck"] == null && HttpContext.Current.Request.QueryString["c"] == null)
+        {
+            // Set the cookie and redirect so we can try to detect it.
+            HttpContext.Current.Session["CookieCheck"] = "1";
+            HttpContext.Current.Response.Redirect("~/default.aspx?c=1", true);
+
+            return;
+        }
+        else
+        {
+            // Detect the cookie.
+            if (Session["CookieCheck"] == null || Session["CookieCheck"].ToString() != "1")
+            {
+                // Cookies are disabled
+                HttpContext.Current.Response.Redirect("~/NoCookies.aspx", true);
+                return;
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("Ok, your browser supports cookies.");
+            }
+        }
+    }
+}
---
+++ NoCookies.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="NoCookies.aspx.cs" Inherits="NoCookies"  ContentType="text/html"   %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    We are sorry but your browser does not support cookies.
+    </div>
+    </form>
+</body>
+</html>
